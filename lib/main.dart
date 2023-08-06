cf1647e7c9f096fa7c21f509641aa653514636ac
puchochiri <doawishfor@naver.com>
2023-08-06 20:29:53 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_myproject01_appbar/screen/appBarExample01.dart';
+import 'package:flutter_myproject01_appbar/screen/BuildALayout.dart';
 /// Flutter code sample for [AppBar].
 
 void main() => runApp(const AppBarApp());
@@ -10,54 +11,9 @@ class AppBarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AppBarExample(),
+      home: BuildALayout(),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget {
-  const AppBarExample({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar Demo'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-          IconButton(
-              icon: const Icon(Icons.navigate_next),
-              tooltip:  'Go to the next page',
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute<void>(
-                    builder: (BuildContext context) {
-                      return Scaffold(
-                        appBar: AppBar(
-                          title: const Text('Next page'),
-                        ),
-                        body:  const Center(
-                          child: Text(
-                            'This is the next page',
-                            style: TextStyle(
-                              fontSize: 24
-                            ),
-                          ),
-                        ),
-                      );
-                    }));
-              },
-          )
-
-        ],
-      ),
-
-    );
-  }
-}
---
+++ lib/screen/todoLiest.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
